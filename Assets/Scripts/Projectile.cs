113c062b6e120ed83e2fd732d8640b663d251f41
Totewa <al399165@uji.es>
2021-10-14 01:03:51 +0200

    El sistema Physics

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
