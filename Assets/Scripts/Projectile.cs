9a80e9ec27a8d73343743426f7fa4d04b0bc81f6
Totewa <al399165@uji.es>
2021-10-14 01:08:31 +0200

    ¿Cómo lanzar el proyectil?

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -12,9 +12,20 @@ public class Projectile : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        
+        public void Launch(Vector2 direction, float force)
+        {
+            rigidbody2d.AddForce(direction * force);
+        }
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            //we also add a debug log to know what the projectile touch
+            Debug.Log("Projectile Collision with " + other.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
